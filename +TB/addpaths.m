19d2a103fd7a1e12fc2bfef493b2f8b60ea1eef8
Wesley Hileman <whileman133@gmail.com>
2023-06-23 16:00:15 -0600

    Create addpaths.m

---
+++ +TB/addpaths.m
@@ -0,0 +1,16 @@
+function addpaths(varargin)
+    %ADDPATHS Add all LMB toolbox directories to the MATLAB path.
+
+    parser = inputParser;
+    parser.addOptional('prefix','gen1',@ischar);
+    parser.parse(varargin{:});
+    prefix = parser.Results.prefix;
+    if ~isempty(prefix)
+        prefix = [upper(prefix) '_'];
+    end
+
+    addpath(genpath(TB.getAbsPath([prefix 'UTILITY'])));
+    addpath(genpath(TB.getAbsPath([prefix 'TFS'])));
+    addpath(genpath(TB.getAbsPath([prefix 'XLSX_CELLDEFS'])));
+    addpath(genpath(TB.getAbsPath([prefix 'MAT_CELLDEFS'])));
+end
