dd7d1de28604173e0cea64e3c9aaa41fc4774b16
Wesley Hileman <whileman133@gmail.com>
2023-06-23 16:54:26 -0600

    warburg sim

--- CH2/SIM_WARBURG/runSim.m
+++ CH2/SIM_WARBURG/runSim.m
@@ -14,8 +14,28 @@ soc0Pct = 100;  % initial cell SOC [%]
 socfPct = 80;   % final cell SOC [%]
 Crate = 1;      % discharge current [C-rate]
 TdegC = 25;     % cell temperature [degC]
+% Values of warburg resistance factor for which to run the simulation.
+W = logspace(-1,1,10);
 
+% Load P2D cell model, convert to WORM.
 p2dm = loadCellModel(modelName);
 worm = convertCellModel(p2dm,'WORM');
 Q = getCellParam(worm,'const.Q');
-simData = simCC(worm,Crate*Q,soc0Pct,socfPct,TdegC,'Verbose',true);
+
+% Run simulations for different W.
+clear WSeries;
+for k = length(W):-1:1
+    p.const.W = W(k);
+    mod = setCellParam(worm,p);
+    WSeries(k) = simCC(mod,Crate*Q,soc0Pct,socfPct,TdegC,'Verbose',true);
+end
+
+simData.W = W;
+simData.WSeries = WSeries;
+simData.p2dm = p2dm;
+simData.worm = worm;
+simData.soc0Pct = soc0Pct;
+simData.socfPct = socfPct;
+simData.Crate = Crate;
+simData.TdegC = TdegC;
+save('warburgSim.mat','simData');
