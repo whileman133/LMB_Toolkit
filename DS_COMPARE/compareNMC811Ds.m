c727bb69d7b8d2593cab84c4f914498a89729e85
Wesley Hileman <whileman133@gmail.com>
2024-04-24 18:22:58 -0600

    Update compareNMC811Ds.m

--- DS_COMPARE/compareNMC811Ds.m
+++ DS_COMPARE/compareNMC811Ds.m
@@ -3,9 +3,18 @@ clear; close all; clc;
 addpath('..');
 TB.addpaths;
 
-RuessDs = readtable('NMC811-Ds-RuessEtAl.xlsx');
-UocpREF = RuessDs.Uocp;
-DsREF_cm2secN1 = RuessDs.Ds;
+% Load Ds vs OCP data from literature reference.
+RefData = readtable('NMC811-Ds-RuessEtAl.xlsx');
+UocpREF = RefData.Uocp;
+DsREF_cm2secN1 = RefData.Ds;
+
+% Load lab OCP.
+warning('off','MATLAB:dispatcher:UnresolvedFunctionHandle');
+ocpData = load( ...
+    fullfile('..','CH6','ocp','labdata','fitstruct',ocpExpirementName));
+[TdegC,indTemp] = max(ocpData.study.testTemperatures); % use highest temp.
+ocpfit = ocpData.study.tests(indTemp);
+warning('on','MATLAB:dispatcher:UnresolvedFunctionHandle');
 
 figure;
 semilogy(UocpREF,DsREF_cm2secN1,'o-');
